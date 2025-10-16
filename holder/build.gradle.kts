2284055621324a7f949b0e62f351d1ae96038076
Michael Chadwick <231858949+michaelchadwick1-GDS@users.noreply.github.com>
2025-10-16 10:21:06 +0100

    chore: Create holder gradle sub-module

---
+++ holder/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+}
+
+    android {
+    namespace = "uk.gov.onelogin.sharing.holder"
+    compileSdk = 36
+
+    defaultConfig {
+    minSdk = 24
+
+      testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+      consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+       release {
+           isMinifyEnabled = false
+           proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+       }
+    }
+    }
+
+  dependencies {
+
+  }
