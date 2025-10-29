90cc5111e3b1e17627982982220b7d4015d7334a
Shabab-gds <220208438+Shabab-GDS@users.noreply.github.com>
2025-10-29 16:51:48 +0000

    chore: add unit test

---
+++ holder/src/main/kotlin/uk/gov/onelogin/sharing/holder/HolderWelcomeScreen.kt
@@ -0,0 +1,21 @@
+package uk.gov.onelogin.sharing.holder
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import uk.gov.onelogin.sharing.holder.engagement.EngagementGenerator
+
+private const val QR_SIZE = 800
+
+@Composable
+fun HolderWelcomeScreen(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        HolderWelcomeText()
+        val engagement = EngagementGenerator()
+        QrCodeImage(
+            modifier = Modifier,
+            data = "mdoc://${engagement.qrCodeEngagement()}",
+            size = QR_SIZE
+        )
+    }
+}
--- holder/src/main/kotlin/uk/gov/onelogin/sharing/holder/HolderWelcomeText.kt
+++ holder/src/main/kotlin/uk/gov/onelogin/sharing/holder/HolderWelcomeText.kt
@@ -1,25 +1,8 @@
 package uk.gov.onelogin.sharing.holder
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import uk.gov.onelogin.sharing.holder.engagement.EngagementGenerator
-
-private const val QR_SIZE = 800
-
-@Composable
-fun HolderWelcomeScreen(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
-        HolderWelcomeText()
-        val engagement = EngagementGenerator()
-        QrCodeImage(
-            modifier = Modifier,
-            data = "mdoc://${engagement.qrCodeEngagement()}",
-            size = QR_SIZE
-        )
-    }
-}
 
 @Composable
 fun HolderWelcomeText(modifier: Modifier = Modifier) {
