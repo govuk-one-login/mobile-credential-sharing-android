389b4c288bb2e552bd2ff75530ff14858c475a1f
Michael Chadwick <231858949+michaelchadwick1-GDS@users.noreply.github.com>
2025-11-14 15:17:57 +0000

    test: Add tests for Holder navigation

--- holder/src/test/kotlin/uk/gov/onelogin/sharing/holder/HolderWelcomeScreenTest.kt
+++ holder/src/test/kotlin/uk/gov/onelogin/sharing/holder/HolderWelcomeScreenTest.kt
@@ -1,41 +1,32 @@
 package uk.gov.onelogin.sharing.holder
 
-import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
-import androidx.compose.ui.test.onNodeWithText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.coroutines.test.runTest
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import uk.gov.onelogin.sharing.holder.presentation.HolderWelcomeScreen
-import uk.gov.onelogin.sharing.verifier.HolderWelcomeTexts.HOLDER_WELCOME_TEXT
-import uk.gov.onelogin.sharing.verifier.QrCodeGenerator.QR_CODE_CONTENT_DESC
+import uk.gov.onelogin.sharing.verifier.HolderWelcomeScreenRule
 
 @RunWith(AndroidJUnit4::class)
 class HolderWelcomeScreenTest {
 
     @get:Rule
-    val composeTestRule = createComposeRule()
+    val composeTestRule = HolderWelcomeScreenRule(createComposeRule())
 
     @Test
-    fun displaysWelcomeText() {
-        composeTestRule.setContent {
-            HolderWelcomeScreen()
+    fun displaysWelcomeText() = runTest {
+        composeTestRule.apply {
+            render()
+            assertWelcomeTextIsDisplayed()
         }
-
-        composeTestRule
-            .onNodeWithText(HOLDER_WELCOME_TEXT)
-            .assertIsDisplayed()
     }
 
     @Test
-    fun displaysQrCode() {
-        composeTestRule.setContent {
-            HolderWelcomeScreen()
+    fun displaysQrCode() = runTest {
+        composeTestRule.apply {
+            render()
+            assertQrCodeIsDisplayed()
         }
-
-        composeTestRule.onNodeWithContentDescription(QR_CODE_CONTENT_DESC)
-            .assertIsDisplayed()
     }
 }
