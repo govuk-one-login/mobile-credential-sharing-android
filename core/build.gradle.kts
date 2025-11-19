b6408223b8c5287e08156fd861fc88eff2897a4d
Shabab-gds <220208438+Shabab-GDS@users.noreply.github.com>
2025-11-19 14:28:56 +0000

    chore: create core module

---
+++ core/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    listOf(
+        libs.plugins.templates.android.library
+    ).forEach { alias(it) }
+}
+
+val androidCompileSdk: Int by rootProject.extra
+val androidMinSdk: Int by rootProject.extra
+val namespacePrefix: String by rootProject.extra
+
+android {
+    namespace = "$namespacePrefix.core"
+    compileSdk = androidCompileSdk
+
+    defaultConfig {
+        minSdk = androidMinSdk
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+mavenPublishingConfig {
+    mavenConfigBlock {
+        name.set(
+            "GOV.UK One Login Wallet Sharing: Core"
+        )
+        description.set(
+            """
+            A module to share common features between modules
+            """.trimIndent()
+        )
+    }
+}
