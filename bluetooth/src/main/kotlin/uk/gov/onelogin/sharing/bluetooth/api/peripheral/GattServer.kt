a2933c381b4792be3b0798cfcd8d770ef40e6bdb
Emanuel Canha <emanuel.canha@digital.cabinet-office.gov.uk>
2025-11-24 21:15:44 +0000

    chore: add gatt service

---
+++ bluetooth/src/main/kotlin/uk/gov/onelogin/sharing/bluetooth/api/peripheral/GattServer.kt
@@ -0,0 +1,5 @@
+package uk.gov.onelogin.sharing.bluetooth.api.peripheral
+
+class GattServer {
+
+}
--- bluetooth/src/main/kotlin/uk/gov/onelogin/sharing/bluetooth/internal/core/AndroidBluetoothAdapterProvider.kt
+++ bluetooth/src/main/kotlin/uk/gov/onelogin/sharing/bluetooth/internal/core/AndroidBluetoothAdapterProvider.kt
@@ -1,8 +1,21 @@
 package uk.gov.onelogin.sharing.bluetooth.internal.core
 
+import android.Manifest
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGattCharacteristic
+import android.bluetooth.BluetoothGattDescriptor
+import android.bluetooth.BluetoothGattServerCallback
+import android.bluetooth.BluetoothGattService
 import android.bluetooth.BluetoothManager
 import android.bluetooth.le.BluetoothLeAdvertiser
 import android.content.Context
+import androidx.annotation.RequiresPermission
+import uk.gov.onelogin.sharing.bluetooth.internal.peripheral.GattUuids.CLIENT_2_SERVER_UUID
+import uk.gov.onelogin.sharing.bluetooth.internal.peripheral.GattUuids.CLIENT_CHARACTERISTIC_CONFIG_UUID
+import uk.gov.onelogin.sharing.bluetooth.internal.peripheral.GattUuids.MDOC_SERVICE_ID
+import uk.gov.onelogin.sharing.bluetooth.internal.peripheral.GattUuids.SERVER_2_CLIENT_UUID
+import uk.gov.onelogin.sharing.bluetooth.internal.peripheral.GattUuids.STATE_UUID
+import java.util.UUID
 
 internal class AndroidBluetoothAdapterProvider(val context: Context) : BluetoothAdapterProvider {
     private val bluetoothManager by lazy {
@@ -13,4 +26,26 @@ internal class AndroidBluetoothAdapterProvider(val context: Context) : Bluetooth
 
     override fun getAdvertiser(): BluetoothLeAdvertiser? =
         bluetoothManager.adapter.bluetoothLeAdvertiser
+
+    val bluetoothGattServerCallback: BluetoothGattServerCallback =
+        object : BluetoothGattServerCallback() {
+            @RequiresPermission(Manifest.permission.BLUETOOTH_CONNECT)
+            override fun onConnectionStateChange(
+                device: BluetoothDevice?,
+                status: Int,
+                newState: Int
+            ) {
+                println("address: ${device?.address}")
+                println("status: $status")
+                println("newState: $newState")
+                device?.uuids?.forEach {
+                    println("uuid: ${it.uuid}")
+                }
+            }
+
+            override fun onServiceAdded(status: Int, service: BluetoothGattService?) {
+                println("status: $status")
+            }
+        }
+
 }
