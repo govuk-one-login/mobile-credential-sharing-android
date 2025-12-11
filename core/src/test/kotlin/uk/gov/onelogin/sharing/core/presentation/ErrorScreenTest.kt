9f89e503ecedb2a1c801471fd4916f38116e9784
Shabab-gds <220208438+Shabab-GDS@users.noreply.github.com>
2025-12-11 11:37:05 +0000

    chore: fix sonar test issue

--- core/src/test/kotlin/uk/gov/onelogin/sharing/core/presentation/ErrorScreenTest.kt
+++ core/src/test/kotlin/uk/gov/onelogin/sharing/core/presentation/ErrorScreenTest.kt
@@ -1,14 +1,13 @@
 package uk.gov.onelogin.sharing.core.presentation
 
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.test.assertHeightIsEqualTo
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import kotlin.test.Test
 import org.junit.Rule
 import org.junit.runner.RunWith
+import kotlin.test.Test
 
 @RunWith(AndroidJUnit4::class)
 class ErrorScreenTest {
@@ -22,6 +21,14 @@ class ErrorScreenTest {
         }
 
         composeTestRule.onNodeWithText("An error has occurred").assertIsDisplayed()
+        composeTestRule.onNodeWithText("An error has occurred").assertExists()
+    }
+
+    @Test
+    fun screenWithTextAnErrorHasOccurredShouldExist() {
+        composeTestRule.setContent {
+            ErrorScreen(modifier = Modifier)
+        }
         composeTestRule.onNodeWithText("An error has occurred").assertExists()
     }
 }
