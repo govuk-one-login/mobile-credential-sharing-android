fda582805b368eda6a81bf32c0d96359d4bb1b5c
Emanuel Canha <emanuel.canha@digital.cabinet-office.gov.uk>
2026-01-21 17:33:37 +0000

    chore: remove unnecessary permissions split manifests

--- bluetooth/src/main/kotlin/uk/gov/onelogin/sharing/bluetooth/api/permissions/PermissionChecker.kt
+++ bluetooth/src/main/kotlin/uk/gov/onelogin/sharing/bluetooth/api/permissions/PermissionChecker.kt
@@ -30,9 +30,8 @@ interface PermissionChecker {
         @JvmStatic
         fun centralPermissions(): List<String> = buildList {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                add(Manifest.permission.BLUETOOTH_SCAN)
                 add(Manifest.permission.BLUETOOTH_CONNECT)
-                add(Manifest.permission.ACCESS_FINE_LOCATION)
+                add(Manifest.permission.BLUETOOTH_SCAN)
             } else {
                 add(Manifest.permission.ACCESS_FINE_LOCATION)
                 add(Manifest.permission.BLUETOOTH)
