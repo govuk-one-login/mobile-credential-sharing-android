0238c2cefb055ad7e34eade4059aa5d9783c7667
Emanuel Canha <emanuel.canha@digital.cabinet-office.gov.uk>
2026-02-10 15:43:41 +0000

    chore: add kdoc for CredentialSharingUiImpl

--- ui/ui-impl/src/main/kotlin/uk/gov/onelogin/sharing/ui/impl/CredentialSharingUiImpl.kt
+++ ui/ui-impl/src/main/kotlin/uk/gov/onelogin/sharing/ui/impl/CredentialSharingUiImpl.kt
@@ -12,7 +12,21 @@ import uk.gov.onelogin.sharing.ui.api.CredentialSharingDestination
 import uk.gov.onelogin.sharing.ui.api.CredentialSharingUi
 import uk.gov.onelogin.sharing.ui.impl.di.CredentialSharingUiGraph
 
+/**
+ * Implementation of [CredentialSharingUi] that provides the entry point for the
+ * Credential Sharing UI.
+ *
+ * This class handles the creation of the UI dependency graph and sets up
+ * the [LocalMetroViewModelFactory] for the UI components.
+ */
 class CredentialSharingUiImpl : CredentialSharingUi {
+    /**
+     * Renders the Credential Sharing UI.
+     *
+     * @param sdk The [CredentialSharingSdk] instance to use for core functionality.
+     * @param startDestination The initial screen to display in the Credential Sharing flow.
+     * @param modifier The [Modifier] to apply to the UI.
+     */
     @Composable
     override fun Render(
         sdk: CredentialSharingSdk,
