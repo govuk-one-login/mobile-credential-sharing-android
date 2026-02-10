323439b1baf28a99dc6b1bd5ff1cc094b110a34a
Michael Chadwick <231858949+michaelchadwick1-GDS@users.noreply.github.com>
2026-02-10 12:39:12 +0000

    feat: Define transitionable verifier states

--- orchestration/src/main/kotlin/uk/gov/onelogin/sharing/orchestration/session/verifier/VerifierSessionStateTransitions.kt
+++ orchestration/src/main/kotlin/uk/gov/onelogin/sharing/orchestration/session/verifier/VerifierSessionStateTransitions.kt
@@ -1,8 +1,12 @@
 package uk.gov.onelogin.sharing.orchestration.session.verifier
 
 import kotlin.reflect.KClass
+import uk.gov.onelogin.sharing.orchestration.session.verifier.VerifierSessionState.Connecting
 import uk.gov.onelogin.sharing.orchestration.session.verifier.VerifierSessionState.NotStarted
 import uk.gov.onelogin.sharing.orchestration.session.verifier.VerifierSessionState.Preflight
+import uk.gov.onelogin.sharing.orchestration.session.verifier.VerifierSessionState.ProcessingEngagement
+import uk.gov.onelogin.sharing.orchestration.session.verifier.VerifierSessionState.ReadyToScan
+import uk.gov.onelogin.sharing.orchestration.session.verifier.VerifierSessionState.Verifying
 
 /**
  * Convenience alias for defining a [Map] of [VerifierSessionState] types to a [Set] of
@@ -20,5 +24,10 @@ typealias VerifierSessionStateTransitions =
 val validVerifierTransitions: VerifierSessionStateTransitions = mapOf(
     NotStarted::class to setOf(
         Preflight::class
-    )
+    ),
+    Preflight::class to emptySet(),
+    ReadyToScan::class to emptySet(),
+    Connecting::class to emptySet(),
+    ProcessingEngagement::class to emptySet(),
+    Verifying::class to emptySet(),
 )
