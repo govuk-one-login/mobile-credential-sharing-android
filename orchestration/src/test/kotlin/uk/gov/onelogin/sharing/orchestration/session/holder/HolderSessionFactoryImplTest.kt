d50ca66c96124261ed6498adeab6817da9ee72e6
Michael Chadwick <231858949+michaelchadwick1-GDS@users.noreply.github.com>
2026-02-16 10:51:37 +0000

    test: Extract `SessionFactory` test logic

--- orchestration/src/test/kotlin/uk/gov/onelogin/sharing/orchestration/session/holder/HolderSessionFactoryImplTest.kt
+++ orchestration/src/test/kotlin/uk/gov/onelogin/sharing/orchestration/session/holder/HolderSessionFactoryImplTest.kt
@@ -1,23 +1,17 @@
 package uk.gov.onelogin.sharing.orchestration.session.holder
 
-import kotlin.test.Test
-import kotlinx.coroutines.test.runTest
-import org.hamcrest.CoreMatchers.equalTo
-import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matcher
 import uk.gov.logging.testdouble.SystemLogger
+import uk.gov.onelogin.sharing.orchestration.session.SessionFactoryImplTest
+import uk.gov.onelogin.sharing.orchestration.session.StateContainer
+import uk.gov.onelogin.sharing.orchestration.session.holder.matchers.HolderSessionStateMatchers.isNotStarted
+import uk.gov.onelogin.sharing.orchestration.session.matchers.StateContainerMatchers.hasCurrentState
 
-class HolderSessionFactoryImplTest {
+class HolderSessionFactoryImplTest :
+    SessionFactoryImplTest<StateContainer<in HolderSessionState>> {
+
     private val logger = SystemLogger()
-    private val sessionFactory = HolderSessionFactory(logger)
-    private val expectedSession = HolderSessionImpl(logger)
-
-    @Test
-    fun `Factory creates clean Session instances`() = runTest {
-        val createdSession = sessionFactory.create()
-
-        assertThat(
-            createdSession.currentState.value,
-            equalTo(expectedSession.currentState.value)
-        )
-    }
+    override val factory: HolderSessionFactory = HolderSessionFactory(logger)
+    override val assertion: Matcher<StateContainer<in HolderSessionState>> =
+        hasCurrentState(isNotStarted())
 }
--- orchestration/src/test/kotlin/uk/gov/onelogin/sharing/orchestration/session/verifier/VerifierSessionFactoryImplTest.kt
+++ orchestration/src/test/kotlin/uk/gov/onelogin/sharing/orchestration/session/verifier/VerifierSessionFactoryImplTest.kt
@@ -1,23 +1,17 @@
 package uk.gov.onelogin.sharing.orchestration.session.verifier
 
-import kotlin.test.Test
-import kotlinx.coroutines.test.runTest
-import org.hamcrest.CoreMatchers.equalTo
-import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matcher
 import uk.gov.logging.testdouble.SystemLogger
+import uk.gov.onelogin.sharing.orchestration.session.SessionFactoryImplTest
+import uk.gov.onelogin.sharing.orchestration.session.StateContainer
+import uk.gov.onelogin.sharing.orchestration.session.matchers.StateContainerMatchers.hasCurrentState
+import uk.gov.onelogin.sharing.orchestration.session.verifier.matchers.VerifierSessionStateMatchers.isNotStarted
 
-class VerifierSessionFactoryImplTest {
+class VerifierSessionFactoryImplTest :
+    SessionFactoryImplTest<StateContainer<in VerifierSessionState>> {
+
     private val logger = SystemLogger()
-    private val sessionFactory = VerifierSessionFactory(logger)
-    private val expectedSession = VerifierSessionImpl(logger)
-
-    @Test
-    fun `Factory creates clean Session instances`() = runTest {
-        val createdSession = sessionFactory.create()
-
-        assertThat(
-            createdSession.currentState.value,
-            equalTo(expectedSession.currentState.value)
-        )
-    }
+    override val factory: VerifierSessionFactory = VerifierSessionFactory(logger)
+    override val assertion: Matcher<StateContainer<in VerifierSessionState>> =
+        hasCurrentState(isNotStarted())
 }
