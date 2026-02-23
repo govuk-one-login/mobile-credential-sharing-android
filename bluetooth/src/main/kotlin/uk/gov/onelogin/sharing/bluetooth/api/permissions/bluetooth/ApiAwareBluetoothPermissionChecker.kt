a1a0efe9da06b6e703a892f7767531c946e06ddf
Michael Chadwick <231858949+michaelchadwick1-GDS@users.noreply.github.com>
2026-02-23 12:26:28 +0000

    feat: Inject BluetoothPermissionChecker

--- bluetooth/src/main/kotlin/uk/gov/onelogin/sharing/bluetooth/api/permissions/bluetooth/ApiAwareBluetoothPermissionChecker.kt
+++ bluetooth/src/main/kotlin/uk/gov/onelogin/sharing/bluetooth/api/permissions/bluetooth/ApiAwareBluetoothPermissionChecker.kt
@@ -2,12 +2,17 @@ package uk.gov.onelogin.sharing.bluetooth.api.permissions.bluetooth
 
 import android.content.Context
 import android.os.Build
+import dev.zacsweers.metro.AppScope
+import dev.zacsweers.metro.ContributesBinding
+import dev.zacsweers.metrox.viewmodel.ViewModelScope
 import uk.gov.onelogin.sharing.bluetooth.api.permissions.PermissionCheckerResult
 
 /**
  * [BluetoothPermissionChecker] implementation that defers to other implementations based on the
  * Android-powered device's [android.os.Build.VERSION.SDK_INT].
  */
+@ContributesBinding(AppScope::class)
+@ContributesBinding(ViewModelScope::class)
 class ApiAwareBluetoothPermissionChecker(private val context: Context) : BluetoothPermissionChecker {
     override fun checkPeripheralPermissions(): PermissionCheckerResult =
         calculateImplementation().checkPeripheralPermissions()
