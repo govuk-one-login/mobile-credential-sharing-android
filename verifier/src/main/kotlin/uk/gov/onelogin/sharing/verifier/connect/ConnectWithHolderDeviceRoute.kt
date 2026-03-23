9444ba4f1308dbf538b65abfd6bcb44bf1b9058f
Oliver Binns <oliver.binns@digital.cabinet-office.gov.uk>
2026-03-23 16:22:00 +0000

    refactor: remove QR code requirement from navigation

--- verifier/src/main/kotlin/uk/gov/onelogin/sharing/verifier/connect/ConnectWithHolderDeviceRoute.kt
+++ verifier/src/main/kotlin/uk/gov/onelogin/sharing/verifier/connect/ConnectWithHolderDeviceRoute.kt
@@ -6,7 +6,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import androidx.navigation.toRoute
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import kotlinx.serialization.Serializable
 import uk.gov.onelogin.sharing.core.implementation.ImplementationDetail
@@ -22,41 +21,35 @@ import uk.gov.onelogin.sharing.verifier.scan.VerifierScanRoute
     ticket = "DCMAW-16955",
     description = "Successful handling of scanned QR code"
 )
-data class ConnectWithHolderDeviceRoute(val base64EncodedEngagement: String) {
-    companion object {
-        /**
-         * [NavGraphBuilder] extension function for configuring a work-in-progress navigation
-         * target.
-         */
-        @OptIn(ExperimentalPermissionsApi::class)
-        fun NavGraphBuilder.configureConnectWithHolderDeviceRoute(
-            onFindError: (String) -> Unit = {}
-        ) {
-            composable<ConnectWithHolderDeviceRoute> { navBackstackEntry ->
-                val arguments: ConnectWithHolderDeviceRoute = navBackstackEntry.toRoute()
-                val context = LocalContext.current
-                ConnectWithHolderDeviceScreen(
-                    base64EncodedEngagement = arguments.base64EncodedEngagement,
-                    onConnectionError = { error: ConnectWithHolderDeviceError ->
-                        errorTitle(context, error)
-                            .let(onFindError::invoke)
-                            .also {
-                                Log.w(
-                                    ConnectWithHolderDeviceRoute::class.java.simpleName,
-                                    "Navigated to error screen: $error"
-                                )
-                            }
-                    }
-                )
-            }
+object ConnectWithHolderDeviceRoute {
+    /**
+     * [NavGraphBuilder] extension function for configuring a work-in-progress navigation
+     * target.
+     */
+    @OptIn(ExperimentalPermissionsApi::class)
+    fun NavGraphBuilder.configureConnectWithHolderDeviceRoute(onFindError: (String) -> Unit = {}) {
+        composable<ConnectWithHolderDeviceRoute> {
+            val context = LocalContext.current
+            ConnectWithHolderDeviceScreen(
+                onConnectionError = { error: ConnectWithHolderDeviceError ->
+                    errorTitle(context, error)
+                        .let(onFindError::invoke)
+                        .also {
+                            Log.w(
+                                ConnectWithHolderDeviceRoute::class.java.simpleName,
+                                "Navigated to error screen: $error"
+                            )
+                        }
+                }
+            )
         }
+    }
 
-        fun NavController.navigateToConnectWithHolderDeviceRoute(uri: String) = navigate(
-            ConnectWithHolderDeviceRoute(uri)
-        ) {
-            popUpTo<VerifierScanRoute> {
-                inclusive = true
-            }
+    fun NavController.navigateToConnectWithHolderDeviceRoute() = navigate(
+        ConnectWithHolderDeviceRoute
+    ) {
+        popUpTo<VerifierScanRoute> {
+            inclusive = true
         }
     }
 }
