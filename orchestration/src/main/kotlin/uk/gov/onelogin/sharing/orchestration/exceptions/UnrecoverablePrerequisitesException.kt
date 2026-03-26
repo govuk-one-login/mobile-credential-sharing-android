15df05afdcf32a4193cbdb2491b7c2389ec75876
Michael Chadwick <231858949+michaelchadwick1-GDS@users.noreply.github.com>
2026-03-26 15:32:34 +0000

    fix: Compilation issues
    - Remove unused exception, now it's been refactored to a
      `SessionErrorReason`.

--- orchestration/src/main/kotlin/uk/gov/onelogin/sharing/orchestration/exceptions/UnrecoverablePrerequisitesException.kt
+++
@@ -1,6 +0,0 @@
-package uk.gov.onelogin.sharing.orchestration.exceptions
-
-import uk.gov.onelogin.sharing.orchestration.prerequisites.MissingPrerequisite
-
-data class UnrecoverablePrerequisitesException(val prerequisiteReason: List<MissingPrerequisite>) :
-    IllegalStateException()
