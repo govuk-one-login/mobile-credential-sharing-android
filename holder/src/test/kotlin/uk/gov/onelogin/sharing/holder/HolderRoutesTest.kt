a0044ad2032e542455335a60d7583398e4d4927c
Michael Chadwick <231858949+michaelchadwick1-GDS@users.noreply.github.com>
2026-05-06 10:19:27 +0100

    test: Unit test HolderRoutes

--- holder/src/test/kotlin/uk/gov/onelogin/sharing/holder/HolderRoutesTest.kt
+++ holder/src/test/kotlin/uk/gov/onelogin/sharing/holder/HolderRoutesTest.kt
@@ -1,27 +1,37 @@
 package uk.gov.onelogin.sharing.holder
 
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.navigation.NavDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.testing.TestNavHostController
-import androidx.navigation.toRoute
+import androidx.savedstate.SavedState
 import androidx.test.annotation.UiThreadTest
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import dev.zacsweers.metrox.viewmodel.LocalMetroViewModelFactory
+import dev.zacsweers.metrox.viewmodel.MetroViewModelFactory
+import io.mockk.every
+import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
-import org.junit.Assert.assertNotNull
+import org.hamcrest.CoreMatchers.endsWith
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.collection.IsCollectionWithSize.hasSize
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import uk.gov.onelogin.sharing.holder.presentation.HolderPresentQrRoute
+import uk.gov.onelogin.sharing.holder.HolderRoutes.configureHolderRoutes
+import uk.gov.onelogin.sharing.holder.prerequisites.HolderPrerequisitesRoute
+import uk.gov.onelogin.sharing.holder.prerequisites.HolderPrerequisitesViewModel
 import uk.gov.onelogin.sharing.holder.presentation.HolderWelcomeScreenRule
+import uk.gov.onelogin.sharing.orchestration.FakeOrchestrator
 
 @RunWith(AndroidJUnit4::class)
 class HolderRoutesTest {
 
-    lateinit var controller: NavHostController
+    private lateinit var controller: NavHostController
 
     @get:Rule
     val composeTestRule = HolderWelcomeScreenRule(
@@ -30,24 +40,49 @@ class HolderRoutesTest {
 
     @Test
     @UiThreadTest
-    fun holderRoutesAreConfigured() = runTest {
+    fun `Configures routes for holder journey`() = runTest {
+        val destinations = mutableListOf<Pair<NavDestination, SavedState?>>()
+        val viewModelFactory: MetroViewModelFactory = mockk(relaxed = true)
+        every {
+            viewModelFactory.create(
+                HolderPrerequisitesViewModel::class,
+                any()
+            )
+        } returns HolderPrerequisitesViewModel(
+            orchestrator = FakeOrchestrator()
+        )
+
         composeTestRule.setContent {
             val context = LocalContext.current
-            controller = TestNavHostController(context)
-            controller.navigatorProvider.addNavigator(ComposeNavigator())
+            controller = TestNavHostController(context).apply {
+                navigatorProvider.addNavigator(ComposeNavigator())
+            }
 
-            NavHost(
-                navController = controller,
-                startDestination = HolderPresentQrRoute
+            controller.addOnDestinationChangedListener { _, destination, arguments ->
+                destinations.add(destination to arguments)
+            }
+
+            CompositionLocalProvider(
+                LocalMetroViewModelFactory provides viewModelFactory
             ) {
-                composable<HolderPresentQrRoute> {}
+                NavHost(
+                    navController = controller,
+                    startDestination = HolderRoutes
+                ) {
+                    configureHolderRoutes(controller)
+                }
             }
         }
 
-        testScheduler.advanceUntilIdle()
+        composeTestRule.waitForIdle()
 
-        val route = controller.currentBackStackEntry?.toRoute<HolderPresentQrRoute>()
-
-        assertNotNull(route)
+        assertThat(
+            destinations,
+            hasSize(1)
+        )
+        assertThat(
+            destinations[0].first.route,
+            endsWith(HolderPrerequisitesRoute.toString())
+        )
     }
 }
