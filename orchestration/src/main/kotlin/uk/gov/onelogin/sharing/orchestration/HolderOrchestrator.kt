c75ca1a11e15f26daf03d5fb67f809a0ded23c4c
Shabab-gds <220208438+Shabab-GDS@users.noreply.github.com>
2026-05-07 08:58:11 +0100

    chore: wip

--- orchestration/src/main/kotlin/uk/gov/onelogin/sharing/orchestration/HolderOrchestrator.kt
+++ orchestration/src/main/kotlin/uk/gov/onelogin/sharing/orchestration/HolderOrchestrator.kt
@@ -269,10 +269,10 @@ class HolderOrchestrator(
                         ImplementationDetail(
                             ticket = "DCMAW-16898",
                             description = "We may need to handle explicit bluetooth " +
-                                "disconnection states to handle common error codes " +
-                                "8, 19, 22 and 133. The function below will handle " +
-                                "treat all disconnect states the same when connected " +
-                                "to a device"
+                                    "disconnection states to handle common error codes " +
+                                    "8, 19, 22 and 133. The function below will handle " +
+                                    "treat all disconnect states the same when connected " +
+                                    "to a device"
                         )
                     ]
                 )
