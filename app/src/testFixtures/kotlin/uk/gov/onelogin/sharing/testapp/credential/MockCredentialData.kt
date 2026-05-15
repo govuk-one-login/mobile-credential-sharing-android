c2a379c8ab4e51bf738ac1ba26f9d08756d46a40
Emanuel Canha <emanuel.canha@digital.cabinet-office.gov.uk>
2026-05-15 17:26:49 +0100

    chore: add mock private key and unit tests

--- app/src/testFixtures/kotlin/uk/gov/onelogin/sharing/testapp/credential/MockCredentialData.kt
+++ app/src/testFixtures/kotlin/uk/gov/onelogin/sharing/testapp/credential/MockCredentialData.kt
@@ -2,4 +2,12 @@ package uk.gov.onelogin.sharing.testapp.credential
 
 object MockCredentialData {
     val mockCredentialState = MockCredentialState(displayName = "Jane Doe")
+
+    val mockPrivateKeyPem = """
+        -----BEGIN PRIVATE KEY-----
+        MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgdpTO3ZY6wCS8ca3H
+        B7OOwwKX+4CRNEvgjReT9NiODBKhRANCAATBOw7zuE5KONXusz2EsQJXICpOwwpW
+        MrZWxlDG/6U1mH8v9LEtfmm4JwMcrYK9Ek0Y19/8FV4SbWyTuiKSNTSa
+        -----END PRIVATE KEY-----
+    """.trimIndent()
 }
