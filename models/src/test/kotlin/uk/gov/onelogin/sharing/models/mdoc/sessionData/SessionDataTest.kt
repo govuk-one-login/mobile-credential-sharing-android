37aaea0b780b322807f40957167a96a3f367c207
Michael Chadwick <231858949+michaelchadwick1-GDS@users.noreply.github.com>
2026-05-18 15:07:18 +0100

    test: Add `SessionDataTest` for functions

---
+++ models/src/test/kotlin/uk/gov/onelogin/sharing/models/mdoc/sessionData/SessionDataTest.kt
@@ -0,0 +1,26 @@
+package uk.gov.onelogin.sharing.models.mdoc.sessionData
+
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class SessionDataTest {
+
+    private val emptySessionData = SessionData()
+    private val fullSessionData = SessionData(
+        data = byteArrayOf(),
+        status = SessionDataStatus.SESSION_TERMINATION
+    )
+
+    @Test
+    fun `Can check if an instance has a 'data' property`() {
+        assertFalse(emptySessionData.hasData())
+        assertTrue(fullSessionData.hasData())
+    }
+
+    @Test
+    fun `Can check if an instance has a 'status' property`() {
+        assertFalse(emptySessionData.hasStatus())
+        assertTrue(fullSessionData.hasStatus())
+    }
+}
