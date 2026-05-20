7fd13ba2afa44035551eaded9a33064e093cb4c0
Michael Chadwick <231858949+michaelchadwick1-GDS@users.noreply.github.com>
2026-05-20 17:07:20 +0100

    feat: Ensure `CertificateValidityPeriod` props

---
+++ verification-module/src/test/kotlin/uk/gov/onelogin/sharing/verification/CertificateValidityPeriodTest.kt
@@ -0,0 +1,26 @@
+package uk.gov.onelogin.sharing.verification
+
+import kotlin.time.ExperimentalTime
+import kotlin.time.Instant
+import org.junit.Test
+import uk.gov.onelogin.sharing.verification.ClassInfoExt.assertInterfaceReturnTypes
+import uk.gov.onelogin.sharing.verification.ClassInfoExt.scanResult
+
+@OptIn(ExperimentalTime::class)
+class CertificateValidityPeriodTest {
+    /**
+     * DCMAW-20245: AC9: [CertificateValidityPeriod] carries `notBefore` and `notAfter` as
+     * timezone-aware timestamps.
+     */
+    @Test
+    fun `Ensure VerifiableDocument constraints`() {
+        val expectedMethods = listOf(
+            "getNotBefore" to Instant::class.java,
+            "getNotAfter" to Instant::class.java
+        )
+
+        val classInfo = scanResult.getClassInfo(CertificateValidityPeriod::class.java.name)
+
+        assertInterfaceReturnTypes(expectedMethods, classInfo)
+    }
+}
