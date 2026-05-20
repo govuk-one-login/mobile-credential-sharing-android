5bec1396871d14c4e92e84f77d752038dcd8374b
Michael Chadwick <231858949+michaelchadwick1-GDS@users.noreply.github.com>
2026-05-20 11:29:20 +0100

    refactor: Tidy unit test

--- verification-module/src/test/kotlin/uk/gov/onelogin/sharing/verification/VerificationResultTest.kt
+++ verification-module/src/test/kotlin/uk/gov/onelogin/sharing/verification/VerificationResultTest.kt
@@ -2,22 +2,25 @@ package uk.gov.onelogin.sharing.verification
 
 import com.google.testing.junit.testparameterinjector.TestParameterInjector
 import io.github.classgraph.ClassGraph
+import io.github.classgraph.ClassInfo
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
-import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(TestParameterInjector::class)
 class VerificationResultTest {
 
-    private val resultInheritorCount = 2
-
     /**
      * DCMAW-20245: AC1: VerificationResult has exactly two states: Success and Failure.
      */
     @Test
     fun `There are only 2 inheritors of 'VerificationResult'`() {
+        val expectedInheritors = listOf(
+            "Failure",
+            "Success"
+        )
+
         val scanResult = ClassGraph()
             .enableAllInfo()
             .acceptPackages(VerificationResult::class.java.packageName)
@@ -26,18 +29,10 @@ class VerificationResultTest {
         val classInfo = scanResult.getClassesImplementing(VerificationResult::class.java)
 
         assertThat(
-            classInfo.size,
-            equalTo(resultInheritorCount)
+            classInfo
+            .map(ClassInfo::getSimpleName)
+            .toSet(),
+            equalTo(expectedInheritors.toSet())
         )
-
-        listOf(
-            "Failure",
-            "Success"
-        ).forEach { expected ->
-            assertTrue(
-                "Cannot find the expected '$expected' inheritor",
-                classInfo.any { expected == it.simpleName }
-            )
-        }
     }
 }
