95b06959151b69738331e398a5ff7f3460f9c0ad
Michael Chadwick <231858949+michaelchadwick1-GDS@users.noreply.github.com>
2026-05-20 12:38:11 +0100

    feat: Ensure `WithPresentation` constraints

--- verification-module/src/test/kotlin/uk/gov/onelogin/sharing/verification/VerifiableDocumentTest.kt
+++ verification-module/src/test/kotlin/uk/gov/onelogin/sharing/verification/VerifiableDocumentTest.kt
@@ -1,11 +1,16 @@
 package uk.gov.onelogin.sharing.verification
 
 import io.github.classgraph.ClassGraph
+import io.github.classgraph.ClassInfo
 import org.hamcrest.CoreMatchers.containsString
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 
 class VerifiableDocumentTest {
+    private val scanResult = ClassGraph()
+        .enableAllInfo()
+        .acceptPackages(VerifiableDocument::class.java.packageName)
+        .scan()
 
     /**
      * DCMAW-20245: AC4: [VerifiableDocument] exposes docType and issuerSigned as defined.
@@ -17,12 +22,34 @@ class VerifiableDocumentTest {
             "getIssuerSigned" to IssuerSigned::class.java
         )
 
-        val classInfo = ClassGraph()
-            .enableAllInfo()
-            .acceptPackages(VerifiableDocument::class.java.packageName)
-            .scan()
-            .getClassInfo(VerifiableDocument::class.java.name)
+        val classInfo = scanResult.getClassInfo(VerifiableDocument::class.java.name)
 
+        assertInterfaceSignatures(expectedMethods, classInfo)
+    }
+
+    /**
+     * DCMAW-20245: AC6: VerifiableDocumentWithPresentation extends VerifiableDocument and
+     * exposes deviceSigned: DeviceSigned
+     */
+    @Test
+    fun `Ensure VerifiableDocument$WithPresentation constraints`() {
+        val expectedMethods = listOf(
+            "getDocType" to String::class.java,
+            "getIssuerSigned" to IssuerSigned::class.java,
+            "getDeviceSigned" to DeviceSigned::class.java,
+        )
+
+        val classInfo = scanResult.getClassInfo(
+            VerifiableDocument.WithPresentation::class.java.name
+        )
+
+        assertInterfaceSignatures(expectedMethods, classInfo)
+    }
+
+    private fun assertInterfaceSignatures(
+        expectedMethods: List<Pair<String, Class<out Any>>>,
+        classInfo: ClassInfo,
+    ) {
         expectedMethods.forEach { (expectedName, expectedType) ->
             val methodInfo = classInfo.methodInfo.getSingleMethod(expectedName)
             assertThat(
