2539364939f9d03bf101317bd24d9ffd25f191c4
Michael Chadwick <231858949+michaelchadwick1-GDS@users.noreply.github.com>
2026-05-22 11:54:32 +0100

    feat: Ensure ISO DocumentVerifier constraints

--- credential-verification/src/main/kotlin/uk/gov/onelogin/sharing/verification/document/Iso18013DocumentVerifier.kt
+++ credential-verification/src/main/kotlin/uk/gov/onelogin/sharing/verification/document/Iso18013DocumentVerifier.kt
@@ -2,6 +2,10 @@ package uk.gov.onelogin.sharing.verification.document
 
 import java.security.cert.X509Certificate
 import uk.gov.onelogin.sharing.models.mdoc.transcript.SessionTranscript
+import uk.gov.onelogin.sharing.verification.document.models.CertificateValidityPeriod
+import uk.gov.onelogin.sharing.verification.document.models.DeviceKeyInfo
+import uk.gov.onelogin.sharing.verification.document.models.MobileSecurityObject
+import uk.gov.onelogin.sharing.verification.document.result.VerificationError
 import uk.gov.onelogin.sharing.verification.document.result.VerificationResult
 import uk.gov.onelogin.sharing.verification.trust.TrustVerifier
 
@@ -15,4 +19,52 @@ class Iso18013DocumentVerifier(
     ): VerificationResult.Success {
         TODO("Not yet implemented")
     }
+
+    /**
+     * @throws VerificationResult.Failure
+     */
+    internal fun decodeMSO(encodedMSO: ByteArray): MobileSecurityObject {
+        throw VerificationResult.Failure(VerificationError.MALFORMED_MSO)
+    }
+
+    /**
+     * @throws VerificationResult.Failure
+     */
+    internal fun verifyMSOFields(document: VerifiableDocument, mso: MobileSecurityObject) {
+        throw VerificationResult.Failure(VerificationError.INVALID_MSO_VERSION)
+    }
+
+    /**
+     * @throws VerificationResult.Failure
+     */
+    internal fun verifyDocumentDigests(document: VerifiableDocument, mso: MobileSecurityObject) {
+        throw VerificationResult.Failure(VerificationError.DIGEST_MISMATCH)
+    }
+
+    /**
+     * @throws VerificationResult.Failure
+     */
+    internal fun verifyValidityInfo(
+        validityPeriod: CertificateValidityPeriod,
+        mso: MobileSecurityObject
+    ) {
+        throw VerificationResult.Failure(VerificationError.VALIDITY_SIGNED_OUT_OF_RANGE)
+    }
+
+    /**
+     * @throws VerificationResult.Failure
+     */
+    internal fun verifyDeviceAuth(
+        document: VerifiableDocument.WithPresentation,
+        sessionTranscript: SessionTranscript,
+        deviceKeyInfo: DeviceKeyInfo
+    ) {
+        throw VerificationResult.Failure(VerificationError.INVALID_DEVICE_SIGNATURE)
+    }
+
+    internal fun buildDeviceAuthenticationBytes(
+        sessionTranscript: SessionTranscript,
+        docType: String,
+        deviceNameSpacesBytes: ByteArray
+    ): ByteArray = byteArrayOf()
 }
--- credential-verification/src/test/kotlin/uk/gov/onelogin/sharing/verification/document/Iso18013DocumentVerifierTest.kt
+++ credential-verification/src/test/kotlin/uk/gov/onelogin/sharing/verification/document/Iso18013DocumentVerifierTest.kt
@@ -1,14 +1,25 @@
 package uk.gov.onelogin.sharing.verification.document
 
+import com.google.testing.junit.testparameterinjector.KotlinTestParameters.testValues
+import com.google.testing.junit.testparameterinjector.TestParameter
+import com.google.testing.junit.testparameterinjector.TestParameterInjector
 import java.security.cert.X509Certificate
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.collection.IsCollectionWithSize.hasSize
 import org.junit.Test
+import org.junit.runner.RunWith
+import uk.gov.onelogin.sharing.models.mdoc.transcript.SessionTranscript
 import uk.gov.onelogin.sharing.verification.ClassInfoExt.scanResult
+import uk.gov.onelogin.sharing.verification.document.models.CertificateValidityPeriod
+import uk.gov.onelogin.sharing.verification.document.models.DeviceKeyInfo
+import uk.gov.onelogin.sharing.verification.document.models.MobileSecurityObject
 import uk.gov.onelogin.sharing.verification.trust.TrustVerifier
 
+@RunWith(TestParameterInjector::class)
 class Iso18013DocumentVerifierTest {
+    private val classInfo = scanResult.getClassInfo(Iso18013DocumentVerifier::class.java.name)
+    private val privateFunctionSuffix = $$"$credential_verification"
 
     /**
      * DCMAW-20246: AC1: [Iso18013DocumentVerifier] compiles, implements [DocumentVerifier],
@@ -16,8 +27,7 @@ class Iso18013DocumentVerifierTest {
      */
     @Test
     fun `Ensure constructor constraints`() {
-        val constructorInfo = scanResult.getClassInfo(Iso18013DocumentVerifier::class.java.name)
-            .constructorInfo
+        val constructorInfo = classInfo.constructorInfo
 
         assertThat(
             constructorInfo,
@@ -30,6 +40,65 @@ class Iso18013DocumentVerifierTest {
                     "${X509Certificate::class.java.simpleName}, " +
                     "${TrustVerifier::class.java.simpleName})"
             )
+        )
+    }
+
+    /**
+     * DCMAW-20246: AC3: 'Private' methods exist on [Iso18013DocumentVerifier] with the correct
+     * signatures
+     */
+    @Test
+    fun `Ensure function count`() {
+        val methodInfo = classInfo.methodInfo
+
+        assertThat(
+            methodInfo,
+            hasSize(7)
+        )
+    }
+
+    /**
+     * DCMAW-20246: AC3: 'Private' methods exist on [Iso18013DocumentVerifier] with the correct
+     * signatures
+     */
+    @Test
+    fun `Ensure private function signature`(
+        @TestParameter functionsToDescriptors: Pair<String, String> = testValues(
+            "decodeMSO" to "${MobileSecurityObject::class.java.name} (byte[])",
+            "verifyMSOFields" to "void (" +
+                    "${VerifiableDocument::class.java.simpleName}, " +
+                    "${MobileSecurityObject::class.java.simpleName}" +
+                    ")",
+            "verifyDocumentDigests" to
+                    "void (" +
+                    "${VerifiableDocument::class.java.simpleName}, " +
+                    "${MobileSecurityObject::class.java.simpleName}" +
+                    ")",
+            "verifyValidityInfo" to
+                    "void (" +
+                    "${CertificateValidityPeriod::class.java.simpleName}, " +
+                    "${MobileSecurityObject::class.java.simpleName}" +
+                    ")",
+            "verifyDeviceAuth" to
+                    "void (" +
+                    "${VerifiableDocument.WithPresentation::class.java.simpleName}, " +
+                    "${SessionTranscript::class.java.simpleName}, " +
+                    "${DeviceKeyInfo::class.java.simpleName}" +
+                    ")",
+            "buildDeviceAuthenticationBytes" to
+                    "${ByteArray::class.java.simpleName} (" +
+                    "${SessionTranscript::class.java.simpleName}, " +
+                    "${String::class.java.simpleName}, " +
+                    "${ByteArray::class.java.simpleName}" +
+                    ")"
+        )
+    ) {
+        val (name, expectedDescriptor) = functionsToDescriptors
+        val methodInfo = classInfo.methodInfo.getSingleMethod("$name$privateFunctionSuffix")
+
+        assertThat(
+            methodInfo.typeDescriptor.toStringWithSimpleNames(),
+            equalTo(expectedDescriptor)
         )
     }
 }
