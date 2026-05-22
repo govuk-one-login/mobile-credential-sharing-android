65dbc34fed4c06e233ae074bbd8884579856bc3a
Michael Chadwick <231858949+michaelchadwick1-GDS@users.noreply.github.com>
2026-05-22 10:06:51 +0100

    docs: Reference which errors get thrown

--- credential-verification/src/main/kotlin/uk/gov/onelogin/sharing/verification/trust/TrustVerifier.kt
+++ credential-verification/src/main/kotlin/uk/gov/onelogin/sharing/verification/trust/TrustVerifier.kt
@@ -3,10 +3,16 @@ package uk.gov.onelogin.sharing.verification.trust
 import java.security.cert.X509Certificate
 import java.security.interfaces.ECPublicKey
 import uk.gov.onelogin.sharing.verification.document.models.CertificateValidityPeriod
+import uk.gov.onelogin.sharing.verification.document.result.VerificationError
+import uk.gov.onelogin.sharing.verification.document.result.VerificationResult
 
 interface TrustVerifier {
     /**
-     * @throws uk.gov.onelogin.sharing.verification.document.result.VerificationResult.Failure
+     * @throws VerificationResult.Failure When [data] cannot be verified, with one of the proceeding
+     * errors:
+     * - [VerificationError.MALFORMED_ISSUER_AUTH]
+     * - [VerificationError.INVALID_ISSUER_SIGNATURE]
+     * - [VerificationError.UNTRUSTED_CERTIFICATE]
      */
     fun verifyCOSESign1(
         data: ByteArray,
