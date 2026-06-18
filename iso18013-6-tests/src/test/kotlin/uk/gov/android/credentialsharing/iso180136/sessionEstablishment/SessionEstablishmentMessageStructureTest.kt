d4b6a2d76606dd4d9ed8522f2f3300a3d80feaee
Michael Chadwick <231858949+michaelchadwick1-GDS@users.noreply.github.com>
2026-06-18 12:06:23 +0100

    test: No indefinite length objects

--- iso18013-6-tests/src/test/kotlin/uk/gov/android/credentialsharing/iso180136/sessionEstablishment/SessionEstablishmentMessageStructureTest.kt
+++ iso18013-6-tests/src/test/kotlin/uk/gov/android/credentialsharing/iso180136/sessionEstablishment/SessionEstablishmentMessageStructureTest.kt
@@ -1,13 +1,24 @@
 package uk.gov.android.credentialsharing.iso180136.sessionEstablishment
 
 import com.fasterxml.jackson.dataformat.cbor.CBORConstants
+import com.fasterxml.jackson.dataformat.cbor.CBORConstants.BYTE_OBJECT_INDEFINITE
+import com.fasterxml.jackson.dataformat.cbor.CBORConstants.BYTE_STRING_INDEFINITE
+import com.fasterxml.jackson.dataformat.cbor.CBORConstants.PREFIX_TYPE_BYTES
+import com.fasterxml.jackson.dataformat.cbor.CBORConstants.SUFFIX_INDEFINITE
+import com.google.testing.junit.testparameterinjector.KotlinTestParameters.namedTestValues
+import com.google.testing.junit.testparameterinjector.TestParameter
+import com.google.testing.junit.testparameterinjector.TestParameterInjector
 import kotlin.test.Test
 import kotlin.test.assertNotNull
 import org.hamcrest.CoreMatchers.allOf
 import org.hamcrest.CoreMatchers.containsString
 import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.CoreMatchers.not
 import org.hamcrest.CoreMatchers.startsWith
+import org.hamcrest.Matcher
 import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.contains
+import org.junit.runner.RunWith
 import uk.gov.onelogin.sharing.models.mdoc.cbor.CborMapper
 import uk.gov.onelogin.sharing.models.mdoc.cbor.HexFormatter
 import uk.gov.onelogin.sharing.models.mdoc.sessionEstablishment.SessionEstablishmentDto
@@ -28,6 +39,7 @@ import uk.gov.onelogin.sharing.models.mdoc.sessionEstablishment.SessionEstablish
  * }
  * ```
  */
+@RunWith(TestParameterInjector::class)
 class SessionEstablishmentMessageStructureTest {
     private val mapper = CborMapper.default
 
@@ -39,8 +51,12 @@ class SessionEstablishmentMessageStructureTest {
         result.toHexString()
     }
 
+    /**
+     * Scenario ID: mDLR_MS_SE_01
+     * sub-scenario: Common_CBOR_01
+     */
     @Test
-    fun `mDLR_MS_SE_01 (Common_CBOR_01) - Valid CBOR is decodable`() {
+    fun `Valid CBOR is decodable`() {
         val result = mapper.readValue(
             validSessionEstablishmentDtoBytes,
             SessionEstablishmentDto::class.java
@@ -49,28 +65,60 @@ class SessionEstablishmentMessageStructureTest {
         assertNotNull(result)
     }
 
+    /**
+     * Scenario ID: mDLR_MS_SE_01
+     * sub-scenario: Common_CBOR_01
+     */
     @Test
-    fun `mDLR_MS_SE_01 (Common_CBOR_01) - Deserialization creates valid CBOR`() {
+    fun `Deserialization creates valid CBOR`() {
         assertThat(
             result,
             equalTo(validSessionEstablishmentDtoBytes)
         )
     }
 
+    /**
+     * Scenario ID: mDLR_MS_SE_01
+     * sub-scenario: Common_CBOR_02
+     */
     @Test
-    fun `mDLR_MS_SE_02 - hex string is a map with 2 elements`() {
+    fun `There are no indefinite length objects`(
+        @TestParameter assertion: Matcher<in String> = namedTestValues(
+            "Indefinite length byte strings" to containsString(
+                BYTE_STRING_INDEFINITE.toHexString()
+            ),
+            "Indefinite byte objects" to containsString(
+                BYTE_OBJECT_INDEFINITE.toHexString()
+            ),
+            "Indefinite byte arrays" to containsString(
+                (PREFIX_TYPE_BYTES + SUFFIX_INDEFINITE).toHexString()
+            )
+        )
+    ) {
+        assertThat(
+            resultHexString.chunked(2),
+            not(contains(assertion))
+        )
+    }
+
+    /**
+     * Scenario: mDLR_MS_SE_02
+     */
+    @Test
+    fun `CBOR structure is a map of 2 elements`() {
         assertThat(
             resultHexString,
             startsWith(HexFormatter(CBORConstants.PREFIX_TYPE_OBJECT + 2))
         )
     }
 
+    /**
+     * Scenario: mDLR_MS_SE_02
+     */
     @Test
-    fun `mDLR_MS_SE_03 - hex string elements are the correct data type`() {
+    fun `eReaderKey is an embedded CBOR object with 4 fields`() {
         val eReaderKeyPrefix = "eReaderKey".toByteArray().toHexString()
-        val dataPrefix = "data".toByteArray().toHexString()
         val embeddedCborPaddingStart = "d818584b"
-        val hStringPrefix = "5902df"
 
         val expectedEReaderPrefix = eReaderKeyPrefix +
                 embeddedCborPaddingStart +
@@ -78,9 +126,21 @@ class SessionEstablishmentMessageStructureTest {
 
         assertThat(
             resultHexString,
+            containsString(expectedEReaderPrefix),
+        )
+    }
+
+    /**
+     * Scenario: mDLR_MS_SE_03
+     */
+    @Test
+    fun `Hex string elements are the correct data type`() {
+        val dataPrefix = "data".toByteArray().toHexString()
+        val hStringPrefix = "5902df"
+
+        assertThat(
+            resultHexString,
             allOf(
-                startsWith(HexFormatter(CBORConstants.PREFIX_TYPE_OBJECT + 2)),
-                containsString(expectedEReaderPrefix),
                 containsString(dataPrefix + hStringPrefix)
             )
         )
