4a5847a00d3bfa5c4deab45ab339235baa4ff53a
Emanuel Canha <emanuel.canha@digital.cabinet-office.gov.uk>
2026-07-07 13:24:34 +0100

    chore: add string tests

--- holder/src/testFixtures/kotlin/uk/gov/onelogin/sharing/holder/awaitingresolution/AwaitingVerifierResolutionScreenRule.kt
+++ holder/src/testFixtures/kotlin/uk/gov/onelogin/sharing/holder/awaitingresolution/AwaitingVerifierResolutionScreenRule.kt
@@ -1,15 +1,21 @@
 package uk.gov.onelogin.sharing.holder.awaitingresolution
 
+import android.content.Context
+import android.content.res.Resources
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.ComposeContentTestRule
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
+import androidx.test.core.app.ApplicationProvider
+import uk.gov.onelogin.sharing.holder.R
 
 class AwaitingVerifierResolutionScreenRule(
-    private val composeTestRule: ComposeContentTestRule = createComposeRule()
+    private val composeTestRule: ComposeContentTestRule = createComposeRule(),
+    private val resources: Resources =
+        ApplicationProvider.getApplicationContext<Context>().resources
 ) : ComposeContentTestRule by composeTestRule {
 
     fun assertTitleIsDisplayed() = onNodeWithText(
-        "Details shared"
+        resources.getString(R.string.holder_awaiting_resolution_title)
     ).assertIsDisplayed()
 }
