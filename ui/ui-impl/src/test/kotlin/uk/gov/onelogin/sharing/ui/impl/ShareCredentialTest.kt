bba74f30262ffd139f81fa317ac92d846a5db919
Michael Chadwick <231858949+michaelchadwick1-GDS@users.noreply.github.com>
2026-07-23 11:33:09 +0100

    test: Move close button tests

--- ui/ui-impl/src/test/kotlin/uk/gov/onelogin/sharing/ui/impl/ShareCredentialTest.kt
+++ ui/ui-impl/src/test/kotlin/uk/gov/onelogin/sharing/ui/impl/ShareCredentialTest.kt
@@ -3,12 +3,16 @@ package uk.gov.onelogin.sharing.ui.impl
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.junit4.v2.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.compose.DialogNavigator
 import androidx.navigation.testing.TestNavHostController
 import androidx.test.annotation.UiThreadTest
+import com.google.testing.junit.testparameterinjector.KotlinTestParameters.namedTestValues
+import com.google.testing.junit.testparameterinjector.TestParameter
 import com.google.testing.junit.testparameterinjector.TestParameters
 import dev.zacsweers.metro.createGraphFactory
+import io.mockk.mockk
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.test.runTest
 import org.junit.Rule
@@ -49,6 +53,55 @@ class ShareCredentialTest {
         }
 
         composeTestRule.waitForIdle()
+    }
+
+    @Test
+    @UiThreadTest
+    fun `Close button exists for incomplete journeys`(
+        @TestParameter state: HolderSessionState = namedTestValues(
+            "not started" to HolderSessionState.NotStarted,
+            "missing prerequisites" to HolderSessionState.Preflight(emptyList()),
+            "Presenting QR code" to HolderSessionState.PresentingEngagement(""),
+            "Awaiting consent" to HolderSessionState.AwaitingUserConsent(mockk(relaxed = true))
+        )
+    ) = runTest {
+        val presenter = FakeCredentialPresenter(
+            appGraph = appGraph,
+            orchestrator = FakeOrchestrator(
+                initialHolderState = MutableStateFlow(state)
+            )
+        )
+
+        composeTestRule.run {
+            setContent { ShareCredential(component = presenter) }
+            waitForIdle()
+            onNodeWithContentDescription("Close").assertExists()
+        }
+    }
+
+    @Test
+    @UiThreadTest
+    fun `Close button doesn't exist for complete journeys`(
+        @TestParameter state: HolderSessionState = namedTestValues(
+            "Cancelled" to HolderSessionState.Complete.Cancelled,
+            "Failed" to HolderSessionState.Complete.Failed(mockk(relaxed = true)),
+            "Success" to HolderSessionState.Complete.Success(
+                HolderSessionState.Complete.SuccessReason.Approved
+            )
+        )
+    ) = runTest {
+        val presenter = FakeCredentialPresenter(
+            appGraph = appGraph,
+            orchestrator = FakeOrchestrator(
+                initialHolderState = MutableStateFlow(state)
+            )
+        )
+
+        composeTestRule.run {
+            setContent { ShareCredential(component = presenter) }
+            waitForIdle()
+            onNodeWithContentDescription("Close").assertDoesNotExist()
+        }
     }
 
     @TestParameters(valuesProvider = HolderStateToNavigationRoute::class)
