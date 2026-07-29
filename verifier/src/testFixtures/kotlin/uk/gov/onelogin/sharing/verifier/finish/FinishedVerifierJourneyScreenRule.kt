cc70ee8bd7782b27fc070db511e2724f9feb5aef
Michael Chadwick <231858949+michaelchadwick1-GDS@users.noreply.github.com>
2026-07-29 12:38:59 +0100

    test: Verifier success - exit call count assertion

--- verifier/src/testFixtures/kotlin/uk/gov/onelogin/sharing/verifier/finish/FinishedVerifierJourneyScreenRule.kt
+++ verifier/src/testFixtures/kotlin/uk/gov/onelogin/sharing/verifier/finish/FinishedVerifierJourneyScreenRule.kt
@@ -5,13 +5,21 @@ import androidx.compose.ui.test.junit4.ComposeContentTestRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.onParent
 import androidx.compose.ui.test.performClick
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.greaterThan
 import uk.gov.onelogin.sharing.verification.format.document.VerifiableDocument
 
 class FinishedVerifierJourneyScreenRule(composeTestRule: ComposeContentTestRule) :
     ComposeContentTestRule by composeTestRule {
     private var hasExitedJourney: Boolean = false
+    private var hasExitedJourneyCount = 0
 
     fun assertHasExitedJourney() = waitUntil { hasExitedJourney }
+
+    fun assertHasExitedJourneyCount(expectedCount: Matcher<in Int> = greaterThan(0)) =
+        waitUntil(
+            "Unexpected exit journey call count! Actual: $hasExitedJourneyCount"
+        ) { expectedCount.matches(hasExitedJourneyCount) }
 
     fun assertDocumentIsDisplayed(document: VerifiableDocument) =
         onNodeWithText(document.toString(), substring = true)
@@ -26,5 +34,6 @@ class FinishedVerifierJourneyScreenRule(composeTestRule: ComposeContentTestRule)
 
     fun updateHasExitedJourney(hasExitedJourney: Boolean = true) {
         this.hasExitedJourney = hasExitedJourney
+        this.hasExitedJourneyCount++
     }
 }
