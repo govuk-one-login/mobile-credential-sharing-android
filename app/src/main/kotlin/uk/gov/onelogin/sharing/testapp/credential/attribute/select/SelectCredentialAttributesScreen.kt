52d6327e96f4825b86abf06af2f03ecdfb3f66b5
Michael Chadwick <231858949+michaelchadwick1-GDS@users.noreply.github.com>
2026-08-20 14:15:55 +0100

    feat: Use dropdown menu for attribute group

--- app/src/main/kotlin/uk/gov/onelogin/sharing/testapp/credential/attribute/select/SelectCredentialAttributesScreen.kt
+++ app/src/main/kotlin/uk/gov/onelogin/sharing/testapp/credential/attribute/select/SelectCredentialAttributesScreen.kt
@@ -1,32 +1,48 @@
 package uk.gov.onelogin.sharing.testapp.credential.attribute.select
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.RadioButton
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.role
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
+import uk.gov.android.ui.theme.m3.GdsTheme
+import uk.gov.android.ui.theme.spacingSingle
 import uk.gov.onelogin.sharing.core.performance.JankStatsHelper.putScreenState
 import uk.gov.onelogin.sharing.core.performance.JankStatsHelper.rememberMetricsStateHolder
 import uk.gov.onelogin.sharing.orchestration.verificationrequest.AttributeGroup
@@ -37,7 +53,7 @@ import uk.gov.onelogin.sharing.testapp.VERIFY_CREDENTIAL_BUTTON_TAG
 @Composable
 internal fun SelectCredentialAttributesScreen(
     modifier: Modifier = Modifier,
-    onSelectAttributeGroup: (AttributeGroup) -> Unit = {}
+    onSelectAttributeGroup: (AttributeGroup) -> Unit = {},
 ) {
     val coroutineScope = rememberCoroutineScope()
     val metrics = rememberMetricsStateHolder()
@@ -48,6 +64,7 @@ internal fun SelectCredentialAttributesScreen(
     var selected by rememberSaveable {
         mutableStateOf(VerifierAttributeOption.PORTRAIT_AND_AGE_OVER_21)
     }
+    var isAttributeGroupExpanded by remember { mutableStateOf(false) }
 
     Surface(
         shape = RoundedCornerShape(24.dp),
@@ -56,33 +73,37 @@ internal fun SelectCredentialAttributesScreen(
     ) {
         Column(
             modifier = Modifier
-                .padding(24.dp).verticalScroll(rememberScrollState())
+                .padding(24.dp)
+                .verticalScroll(rememberScrollState())
         ) {
-            Text(
-                text = stringResource(R.string.select_attribute_group),
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(bottom = 16.dp)
-            )
-            VerifierAttributeOption.entries.forEach { option ->
-                OutlinedButton(
-                    onClick = { coroutineScope.launch { selected = option } },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 4.dp)
-                        .testTag(ATTRIBUTE_GROUP_ITEM_TAG),
-                    border = BorderStroke(
-                        width = if (selected == option) 2.dp else 1.dp,
-                        color = if (selected == option) Color.Blue else Color.Gray
-                    )
-                ) {
-                    RadioButton(
-                        selected = selected == option,
-                        onClick = { coroutineScope.launch { selected = option } }
-                    )
-                    Text(option.displayName)
+
+            UserDropdownMenu(
+                modifier = Modifier.fillMaxWidth().testTag("attribute_group_menu"),
+                label = { Text("Attribute group") },
+                onTextFieldClickLabel = "Select attribute group",
+                textFieldValue = selected.displayName,
+                isAttributeGroupExpanded = isAttributeGroupExpanded,
+                onAttributeGroupExpandedChanged = { isAttributeGroupExpanded = it },
+                dropdownMenuContents = {
+                    Column(
+                        verticalArrangement = Arrangement.spacedBy(spacingSingle)
+                    ) {
+                        VerifierAttributeOption.entries.forEach { option ->
+                            DropdownMenuItem(
+                                modifier = Modifier
+                                    .padding(vertical = 2.dp)
+                                    .testTag(ATTRIBUTE_GROUP_ITEM_TAG),
+                                text = { Text(option.displayName) },
+                                onClick = {
+                                    isAttributeGroupExpanded = false
+                                    selected = option
+                                },
+                            )
+                        }
+                    }
                 }
-            }
+            )
+
             Button(
                 onClick = {
                     coroutineScope.launch {
@@ -95,6 +116,80 @@ internal fun SelectCredentialAttributesScreen(
                     .testTag(VERIFY_CREDENTIAL_BUTTON_TAG)
             ) {
                 Text(stringResource(R.string.verify_credential))
+            }
+        }
+    }
+}
+
+@Composable
+private fun UserDropdownMenu(
+    textFieldValue: String,
+    isAttributeGroupExpanded: Boolean,
+    dropdownMenuContents: @Composable ColumnScope.() -> Unit,
+    modifier: Modifier = Modifier,
+    textFieldColors: TextFieldColors = OutlinedTextFieldDefaults.colors(
+        unfocusedBorderColor = MaterialTheme.colorScheme.onSurface,
+        focusedBorderColor = MaterialTheme.colorScheme.onSurfaceVariant,
+    ),
+    label: @Composable () -> Unit = {},
+    onTextFieldClickLabel: String? = null,
+    onAttributeGroupExpandedChanged: (Boolean) -> Unit = {},
+) {
+    val attributeGroupTrailingIcon by remember {
+        derivedStateOf {
+            if (isAttributeGroupExpanded) {
+                R.drawable.outline_arrow_drop_up_24
+            } else {
+                R.drawable.outline_arrow_drop_down_24
+            }
+        }
+    }
+
+    Box(
+        modifier = modifier
+    ) {
+        OutlinedTextField(
+            colors = textFieldColors,
+            value = textFieldValue,
+            onValueChange = { },
+            label = label,
+            readOnly = true,
+            trailingIcon = {
+                Icon(
+                    painterResource(attributeGroupTrailingIcon),
+                    contentDescription = null
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .testTag("dropdown_text")
+        )
+        Box(
+            modifier = Modifier
+                .matchParentSize()
+                .testTag("expand_dropdown")
+                .clickable(
+                    role = Role.Button,
+                    onClickLabel = onTextFieldClickLabel
+                ) { onAttributeGroupExpandedChanged(!isAttributeGroupExpanded) }
+        )
+
+        DropdownMenu(
+            containerColor = MaterialTheme.colorScheme.surface,
+            expanded = isAttributeGroupExpanded,
+            border = BorderStroke(
+                Dp.Hairline,
+                MaterialTheme.colorScheme.onSurface
+            ),
+            onDismissRequest = { onAttributeGroupExpandedChanged(false) },
+            modifier = Modifier
+                .semantics {
+                    role = Role.Button
+                }
+                .clickable { onAttributeGroupExpandedChanged(!isAttributeGroupExpanded) }
+        ) {
+            GdsTheme {
+                dropdownMenuContents()
             }
         }
     }
--- app/src/testFixtures/kotlin/uk/gov/onelogin/sharing/testapp/credential/attribute/select/SelectCredentialAttributesScreenRule.kt
+++ app/src/testFixtures/kotlin/uk/gov/onelogin/sharing/testapp/credential/attribute/select/SelectCredentialAttributesScreenRule.kt
@@ -2,20 +2,24 @@ package uk.gov.onelogin.sharing.testapp.credential.attribute.select
 
 import android.content.Context
 import android.content.res.Resources
-import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.assertTextContains
+import androidx.compose.ui.test.hasAnyChild
+import androidx.compose.ui.test.hasParent
+import androidx.compose.ui.test.hasTestTag
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.ComposeContentTestRule
 import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.onSibling
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performScrollTo
 import androidx.test.core.app.ApplicationProvider
 import uk.gov.onelogin.sharing.orchestration.verificationrequest.AttributeGroup
+import uk.gov.onelogin.sharing.testapp.ATTRIBUTE_GROUP_ITEM_TAG
 import uk.gov.onelogin.sharing.testapp.R
 
 class SelectCredentialAttributesScreenRule(
     composeTestRule: ComposeContentTestRule,
     private val resources: Resources = ApplicationProvider.getApplicationContext<Context>()
-        .resources
+        .resources,
 ) : ComposeContentTestRule by composeTestRule {
     private var confirmedAttributeGroup: AttributeGroup? = null
 
@@ -23,17 +27,30 @@ class SelectCredentialAttributesScreenRule(
         group == confirmedAttributeGroup
     }
 
-    fun assertOptionIsSelected(option: VerifierAttributeOption) = onVerifierOptionText(option)
-        .onSibling()
-        .assertIsSelected()
+    fun assertOptionIsSelected(option: VerifierAttributeOption) = onNode(
+        hasParent(
+            hasTestTag("attribute_group_menu")
+        ) and hasTestTag("dropdown_text")
+    ).assertTextContains(option.displayName)
 
-    fun onVerifierOptionText(option: VerifierAttributeOption) = onNodeWithText(
-        option.displayName,
+    fun onVerifierOptionText(option: VerifierAttributeOption) = onNode(
+        hasTestTag(
+            ATTRIBUTE_GROUP_ITEM_TAG
+        ) and hasAnyChild(hasText(option.displayName)),
         useUnmergedTree = true
     )
 
-    fun performOptionClick(option: VerifierAttributeOption) = onVerifierOptionText(option)
-        .performClick()
+    fun performAttributeGroupMenuClick() = onNode(
+        hasParent(
+            hasTestTag("attribute_group_menu")
+        ) and hasTestTag("expand_dropdown"),
+        useUnmergedTree = true
+    ).performScrollTo().performClick()
+
+    fun performAttributeGroupClick(option: VerifierAttributeOption) {
+        performAttributeGroupMenuClick()
+        onVerifierOptionText(option).performClick()
+    }
 
     fun performVerifyCredentialClick() = onNodeWithText(
         resources.getString(R.string.verify_credential),
