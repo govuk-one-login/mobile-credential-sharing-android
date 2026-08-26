c608e01662955aec14d5f0c21397ed4a5129f79f
Michael Chadwick <231858949+michaelchadwick1-GDS@users.noreply.github.com>
2026-08-26 15:07:09 +0100

    test: ReaderAuthOption

---
+++ app/src/test/kotlin/uk/gov/onelogin/sharing/testapp/credential/attribute/select/ReaderAuthOptionTest.kt
@@ -0,0 +1,37 @@
+package uk.gov.onelogin.sharing.testapp.credential.attribute.select
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.google.testing.junit.testparameterinjector.TestParameter
+import java.io.IOException
+import kotlin.test.Test
+import kotlin.test.fail
+import kotlinx.coroutines.test.runTest
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestParameterInjector
+
+@RunWith(RobolectricTestParameterInjector::class)
+class ReaderAuthOptionTest(
+    @TestParameter val option: ReaderAuthOption
+) {
+    val context: Context by lazy {
+        ApplicationProvider.getApplicationContext()
+    }
+
+    @Test
+    fun `Certificate file exists`() = runTest {
+        openAsset(option.leafCertificateAssetFileName)
+
+    }
+
+    @Test
+    fun `Private key file exists`() = runTest {
+        openAsset(option.leafCertificatePrivateKeyFileName)
+    }
+
+    private fun openAsset(fileName: String) = try {
+        context.assets.open(fileName)
+    } catch (exception: IOException) {
+        fail("Couldn't open '${fileName}'", exception)
+    }
+}
