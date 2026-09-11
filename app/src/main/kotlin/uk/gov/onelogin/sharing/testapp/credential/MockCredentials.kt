e1e19c60f94dc482d1360699fedfd90fceb6cfbd
Emanuel Canha <emanuel.canha@digital.cabinet-office.gov.uk>
2026-09-11 18:01:06 +0100

    chore: add Jane Doe (absent x5t) credential

--- app/src/main/kotlin/uk/gov/onelogin/sharing/testapp/credential/MockCredentials.kt
+++ app/src/main/kotlin/uk/gov/onelogin/sharing/testapp/credential/MockCredentials.kt
@@ -1,10 +1,16 @@
 package uk.gov.onelogin.sharing.testapp.credential
 
+import uk.gov.onelogin.sharing.testapp.R
+
 object MockCredentials {
 
     fun getMockCredentialStates(): List<MockCredentialState> = listOf(
         MockCredentialState(
             displayName = "Jane Doe"
+        ),
+        MockCredentialState(
+            displayName = "Jane Doe (absent x5t)",
+            rawCredentialRes = R.raw.mock_credential_absent_x5t
         )
     )
 }
