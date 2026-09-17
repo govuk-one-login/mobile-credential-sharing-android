bb5b5bd40a6e54691e154007e53784851ff7b4a7
Shabab-gds <220208438+Shabab-GDS@users.noreply.github.com>
2026-09-17 16:21:04 +0100

    chore: update test fake

--- app/src/testFixtures/kotlin/uk/gov/onelogin/sharing/testapp/FakeCredentialSharingSdk.kt
+++ app/src/testFixtures/kotlin/uk/gov/onelogin/sharing/testapp/FakeCredentialSharingSdk.kt
@@ -1,5 +1,8 @@
 package uk.gov.onelogin.sharing.testapp
 
+import java.security.cert.X509Certificate
+import uk.gov.onelogin.sharing.orchestration.CredentialProvider
+import uk.gov.onelogin.sharing.sdk.api.presenter.CredentialPresenter
 import uk.gov.onelogin.sharing.sdk.api.presenter.PresentCredentialSdk
 import uk.gov.onelogin.sharing.sdk.api.shared.CredentialSharingAppGraph
 import uk.gov.onelogin.sharing.sdk.api.shared.CredentialSharingSdk
@@ -7,6 +10,17 @@ import uk.gov.onelogin.sharing.sdk.api.verifier.VerifyCredentialSdk
 
 class FakeCredentialSharingSdk(
     override val appGraph: CredentialSharingAppGraph,
+    @Suppress("DEPRECATION")
+    @get:Deprecated("Use createCredentialPresenter instead")
     override val presentCredentialSdk: PresentCredentialSdk,
-    override val verifyCredentialSdk: VerifyCredentialSdk
-) : CredentialSharingSdk
+    override val verifyCredentialSdk: VerifyCredentialSdk,
+    private val credentialPresenter: CredentialPresenter? = null
+) : CredentialSharingSdk {
+    override fun createCredentialPresenter(
+        credentialProvider: CredentialProvider,
+        trustedReaderCertificates: List<X509Certificate>
+    ): CredentialPresenter = credentialPresenter
+        ?:
+        @Suppress("DEPRECATION")
+        presentCredentialSdk.presenter(credentialProvider)
+}
